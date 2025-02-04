c275231b5f5a660a1c8ad3047149b741bf198655
eardila <eardila.code@gmail.com>
2025-02-03 20:57:05 -0500

    challenge 5. finding the prime numbers. keep trying

--- src/logic_challenges_practice/Prime_numbers.java
+++ src/logic_challenges_practice/Prime_numbers.java
@@ -19,14 +19,30 @@ public class Prime_numbers {
 		 * Output => [2, 3, 5]
 		 */
 		
-		Vector<Integer> v = new Vector<Integer>();
+		Scanner user_input = new Scanner(System.in);
+		Vector<Integer> primes = new Vector<Integer>();
 		
-		for (int i = 0 ; i < 6 ; i++) {
-			v.addElement(i);
+		primes.addElement(2);
+		System.out.println("Enter the number: ");
+		int num = user_input.nextInt();
+		int com = 0;
+		
+		if (num == 1 || num < 1) {
+			System.out.println("The number " + num + " is not prime");
 		}
-		
-		System.out.println(v);
-		System.out.println(v.get(2));
+		else if (num == 2) {
+			System.out.println("The primes are: " + primes);
+		}
+		else {
+			for (int i = 3 ; i <= num ; i++) {
+				for (int n = 2 ; i <= 9 ; i++) {
+					if (i%n == 0) {com++;}
+				}
+				if (com == 1) {primes.addElement(i);}
+				com = 0;
+			}
+			System.out.println("The primes are: " + primes);
+		}
 	}
-
 }
+
